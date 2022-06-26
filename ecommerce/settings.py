e24cffb9e9d697fb3f039c012b12b0d6bacf9ce9
noufida <noufidap@gmail.com>
2022-06-27 01:46:26 +0530

    added static root

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'asset'
 STATICFILES_DIRS =[
-    os.path.join( BASE_DIR / "static")
+    os.path.join( BASE_DIR , "static")
 ]
 
 
